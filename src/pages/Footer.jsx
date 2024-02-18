d7cfda7969de62e4474df810a7b07b076b64c1f5
emereson <escanorrosa123@gmail.com>
2024-02-17 23:52:39 -0500

    hola

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
         <p>FOLLOW OUR WORK ON OUR SOCIAL NETWORKS!</p>
       </section>
       <section className="footer__sectionTwo">
-        <img src="/logo2.png" alt="" />
+        <img src="/logo2.webp" alt="" />
         <a
           href="https://idotcode.com"
           target="_blank"
